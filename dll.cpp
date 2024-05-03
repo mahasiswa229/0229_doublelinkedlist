fa4c9fc8e292a2fefea7622268259f2915543b74
mahasiswa229 <161399711+mahasiswa229@users.noreply.github.com>
2024-05-03 17:10:54 +0800

    pt2 ngisi main prog

--- dll.cpp
+++ dll.cpp
@@ -182,7 +182,21 @@ int main()
     {
         try
         {
-            
+            cout << "\nMenu" << endl;
+            cout << "1. Add a record to the list" << endl;
+            cout << "2. Delete a record from the list" << endl;
+            cout << "3. View all records in the ascending order of rol numbers" << endl;
+            cout << "4. View all records in the descending order of rol numbers" << endl;
+            cout << "5. Search for a record in the list" << endl;
+            cout << "6. Exit" << endl;
+            cout << "\nEnter your choice(1-6): "
+            char ch;
+            cin ch;
+
+            switch(ch)
+            {
+                case '1':
+            }
         }
     }
 }
