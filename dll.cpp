ea106e57de06274a4efb02adae6d88c7df69cb67
mahasiswa229 <161399711+mahasiswa229@users.noreply.github.com>
2024-05-03 16:44:11 +0800

    step 2 prosedur deletenode

--- dll.cpp
+++ dll.cpp
@@ -83,14 +83,43 @@ void deleteNode()
 
     if(START = NULL)
     {
-        cout << "\nList is empty.";
+        cout << "List is empty." << endl;
         return;
     }
 
     current = START;
     previous = NULL;
 
-    
+    while(current != NULL && current->noMhs != rollNo)
+    {
+        previous = current;
+        current = current->next;
+    }
+
+    if(current == NULL)
+    {
+        cout << "\033[31mDuplicate roll numbers not allowed\033[0m" << endl;
+        return;
+    }
+
+    if(current == START)
+    {
+        START = START->next;
+        if(START != NULL)
+        {
+            START->prev = NULL;
+        }
+    }
+    else
+    {
+        previous->next = current->next;
+        if(current->next != NULL)
+        {
+            current->next->prev = previous;
+        }
+    }
+    delete current;
+    cout << "\x1b[32mRecord with roll number" << rollNo << "deleted\x1b[0m" endl;
 }
 
 int main()
